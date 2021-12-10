888631a223b2e012ce168bccc08d7ea160e3c9b2
sum chanthou <chanthousum2017@gmail.com>
2021-12-10 15:19:55 +0700

    new project

---
+++ Main.py
@@ -0,0 +1,40 @@
+# print("Welcome to SETEC")
+# id=1
+# productName="ABC"
+# # price=3.5
+# # print("ID:"+str(id))
+# # print("Product Name :"+productName)
+# # print("Price :"+str(price))
+# # print(type(price))
+# # num1=1
+# # num2="2"
+# # num3=3.4
+# # total=0
+# # total=num1 + int(num2)+ float(num3);
+# # print("Total :"+str(total))
+# # str1="Python"
+# # str2="Programming"
+# # str3=str1 +" "+ str2
+# # print(str3)
+# price=10
+# unitPrice=3.5
+# print("Product Name :",productName,"Price :",price)
+# print("Name :%s Price:%d untiPrice:%.2f"%(productName,price,unitPrice))
+id=int(input("Enter Id:"))
+productName=str(input("Enter productName :"))
+qty=int(input("Enter qty:"))
+price=float(input("Enter Price:"))
+amount=qty*price;
+print("------------------------")
+print("ID:",id)
+print("Product Name : ",productName)
+print("Qty :",qty)
+print("Price :%.2f"%price)
+print("Amount :%.2f"%(amount))
+
+
+
+
+
+
+
